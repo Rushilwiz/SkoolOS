24bbe6f466aa23ebaa19aad93b6650a9ac5593c0
rushilwiz <rushilwiz@gmail.com>
2020-06-04 22:57:45 -0400

    working

--- CLI/skoolos.py
+++ CLI/skoolos.py
@@ -1,17 +1,30 @@
 import sys
 import click
 from selenium import webdriver;
+import os.path
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-print("")
-print(" SSSSS   CCCCC  HH   HH  OOOOO   OOOOO  LL        OOOOO   SSSSS ")
-print("SS      CC    C HH   HH OO   OO OO   OO LL       OO   OO SS     ")
-print(" SSSSS  CC      HHHHHHH OO   OO OO   OO LL       OO   OO  SSSSS ")
-print("     SS CC    C HH   HH OO   OO OO   OO LL       OO   OO      SS")
-print(" SSSSS   CCCCC  HH   HH  OOOO0   OOOO0  LLLLLLL   OOOO0   SSSSS ")
-print("")
+def main():
+    print("")
+    print(" SSSSS   CCCCC  HH   HH  OOOOO   OOOOO  LL        OOOOO   SSSSS ")
+    print("SS      CC    C HH   HH OO   OO OO   OO LL       OO   OO SS     ")
+    print(" SSSSS  CC      HHHHHHH OO   OO OO   OO LL       OO   OO  SSSSS ")
+    print("     SS CC    C HH   HH OO   OO OO   OO LL       OO   OO      SS")
+    print(" SSSSS   CCCCC  HH   HH  OOOO0   OOOO0  LLLLLLL   OOOO0   SSSSS ")
+    print("")
 
-driver = webdriver.Chrome(PATH)
+    if not os.path.exists(".profile"):
+        authenticate()
+    else:
+        print("welcome back")
 
-driver.get("")
+
+
+def authenticate():
+    file = open(".profile", "w")
+    file.write("Your Username")
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
