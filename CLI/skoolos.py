1af9343228681637e56365bd822c73106f1c4dff
Raffu Khondaker <2022rkhondak@tjhsst.edu>
2020-06-14 14:27:46 -0400

    cypt

--- CLI/skoolos.py
+++ CLI/skoolos.py
@@ -14,6 +14,7 @@ from PyInquirer import prompt, print_json
 import json
 import os
 import argparse
+from cryptography.fernet import Fernet
 
 client_id = r'QeZPBSKqdvWFfBv1VYTSv9iFGz5T9pVJtNUjbEr6'
 client_secret = r'0Wl3hAIGY9SvYOqTOLUiLNYa4OlCgZYdno9ZbcgCT7RGQ8x2f1l2HzZHsQ7ijC74A0mrOhhCVeZugqAmOADHIv5fHxaa7GqFNtQr11HX9ySTw3DscKsphCVi5P71mlGY'
@@ -71,13 +72,14 @@ def authenticate():
     server.start()
 
     browser.get("localhost:8000/")
-
-    while "http://localhost:8000/?code" not in browser.current_url:
+    print(1)
+    while "localhost:8000/callback/?code" not in browser.current_url:
         time.sleep(0.25)
 
     url = browser.current_url
-    gets = url_decode(url.replace("http://localhost:8000/?", ""))
+    gets = url_decode(url.replace("localhost:8000/callback/?", ""))
     code = gets.get("code")
+    print(2)
     if state == gets.get("state"):
         state = gets.get("state")
         print("states good")
@@ -104,6 +106,10 @@ def authenticate():
     os.chdir(cdir)
     profileFile = open(".profile", "w")
     #profileFile.write(profile.text())
+    key = Fernet.generate_key()
+    file = open('key.key', 'wb')
+    file.write(key) # The key is type bytes still
+    file.close()
     profileFile.write(str(profile))
     profileFile.close()
 
